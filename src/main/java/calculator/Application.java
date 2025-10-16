e28af9e9e08a66f482efcafe612b37aee84ed525
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-10-16 23:13:25 +0900

    feat: main try_catch 제거

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,13 +6,9 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = readLine();
 
-        try {
-            int result = add(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            // 요구사항에 따라 예외 발생 시 애플리케이션이 종료
-            System.err.println("에러가 발생했습니다: " + e.getMessage());
-        }
+        int result = add(input);
+        System.out.println("결과 : " + result);
+        
     }
 
 
