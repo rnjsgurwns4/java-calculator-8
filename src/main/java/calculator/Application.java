717486f4bf7612406050a33518c77270451e8c32
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-10-16 22:54:29 +0900

    feat(application): main 함수 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,18 @@
 package calculator;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+
+        try {
+            int result = add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            // 요구사항에 따라 예외 발생 시 애플리케이션이 종료
+            System.err.println("에러가 발생했습니다: " + e.getMessage());
+        }
     }
 }
+
